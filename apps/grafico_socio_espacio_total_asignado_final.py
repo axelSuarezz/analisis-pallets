cb92f2a1b977e5fe6c3bcdf573c404ce97a8c1ec
axel <axeldavalos@hotmail.com>
2025-06-10 11:07:30 -0300

    ruta relativa

--- apps/grafico_socio_espacio_total_asignado_final.py
+++ apps/grafico_socio_espacio_total_asignado_final.py
@@ -61,7 +61,7 @@ st.set_page_config(
 
 @st.cache_data
 def load_data():
-    ruta = 'C:/Users/Sistemas/Desktop/pallets_cruzados_nuevo/test/merge/espacio_total_asignado_final.xlsx'
+    ruta = os.path.join(os.path.dirname(__file__), 'datos', 'espacio_total_asignado_final.xlsx')
     df = pd.read_excel(ruta)
     df['fecha'] = pd.to_datetime(df['fecha'])
     df['mes_año'] = df['fecha'].dt.strftime('%B %Y').str.title()
